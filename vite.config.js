3a6c23c1f66c69640c9fcc5cc36ccd72a19cb184
isaacdemilade8-creator <isaacdemilade8@gmail.com>
2026-04-30 13:48:29 +0100

    live version fix three

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/welcome.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/add-question.css',
+                'resources/css/dashboard.css',
+                'resources/css/leaderboard.css',
+                'resources/css/results.css',
+                'resources/css/student-dashboard.css',
+                'resources/css/style.css',
+                'resources/css/welcome.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
